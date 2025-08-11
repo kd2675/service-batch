9bf5260c104b2d477d278b403752735c8d970ff5
김도영 <zz01064842675@gmail.com>
2025-08-11 14:05:57 +0900

    배치 서비스

---
+++ src/main/java/com/service/batch/servicebatch/ServiceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.service.batch.servicebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBatchApplication.class, args);
+    }
+
+}
