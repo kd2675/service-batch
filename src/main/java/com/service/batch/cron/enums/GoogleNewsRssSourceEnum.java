4ebe8f4c72597adc03c66a1bacf04695097cf38a
김도영 <zz01064842675@gmail.com>
2026-06-02 14:00:50 +0900

    feat: 다양한 RSS 소스 추가 및 기존 소스 확장

--- src/main/java/com/service/batch/cron/enums/GoogleNewsRssSourceEnum.java
+++ src/main/java/com/service/batch/cron/enums/GoogleNewsRssSourceEnum.java
@@ -11,7 +11,14 @@ import java.util.List;
 public enum GoogleNewsRssSourceEnum {
     CNN("googleNewsCnn", "rss-us", "https://news.google.com/rss/search?q=site%3Acnn.com&hl=en-US&gl=US&ceid=US%3Aen"),
     REUTERS("googleNewsReuters", "rss-us", "https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen"),
-    AP("googleNewsAp", "rss-us", "https://news.google.com/rss/search?q=site%3Aapnews.com&hl=en-US&gl=US&ceid=US%3Aen");
+    AP("googleNewsAp", "rss-us", "https://news.google.com/rss/search?q=site%3Aapnews.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    POLITICO("googleNewsPolitico", "rss-us", "https://news.google.com/rss/search?q=site%3Apolitico.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    AXIOS("googleNewsAxios", "rss-us", "https://news.google.com/rss/search?q=site%3Aaxios.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    WASHINGTON_POST("googleNewsWashingtonPost", "rss-us", "https://news.google.com/rss/search?q=site%3Awashingtonpost.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    USA_TODAY("googleNewsUsaToday", "rss-us", "https://news.google.com/rss/search?q=site%3Ausatoday.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    NEWSMAX("googleNewsNewsmax", "rss-us", "https://news.google.com/rss/search?q=site%3Anewsmax.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    TIME("googleNewsTime", "rss-us", "https://news.google.com/rss/search?q=site%3Atime.com&hl=en-US&gl=US&ceid=US%3Aen"),
+    WALL_STREET_JOURNAL("googleNewsWsj", "rss-business", "https://news.google.com/rss/search?q=site%3Awsj.com&hl=en-US&gl=US&ceid=US%3Aen");
 
     private String source;
     private String category;
